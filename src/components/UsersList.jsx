5e3d078a701c91f00d2e1e40166222112c67cd18
Diego Garay <diegogaraycullas@gmail.com>
2022-11-11 22:54:29 -0500

    estilos en las listas

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 const UsersList = ({usersList, deleteUser, selectUser}) => {
   return (
-    <ul >
+    <ul className='m-7'>
       {
         usersList.map(user => (
-          <li key={user.id}>
-            <h3><b>{user.first_name} {user.last_name}</b></h3>
-            <p>{user.email}</p>
-            <p>{user.birthday}</p>
-            <button onClick={() =>deleteUser(user.id)}>Delete</button>
-            <button onClick={()=> selectUser(user)}>Edit</button>
+          <li 
+            key={user.id} 
+            className='list-none bg-slate-300 mb-4 p-5 flex gap-2'>
+            <div className='w-[700px]'>
+              <h3><b>{user.first_name} {user.last_name}</b></h3>
+              <p>{user.email}</p>
+              <p>{user.birthday}</p>
+            </div>
+            <div>
+              <button onClick={() =>deleteUser(user.id)}>Delete</button>
+              <button onClick={()=> selectUser(user)}>Edit</button>
+            </div>
+            
           </li>
         ))
       }
